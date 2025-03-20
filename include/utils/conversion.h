3d725de1a8181b7857638cca106062f28392b8d9
Joe <joe.luke.manning@gmail.com>
2025-03-20 11:11:03 +0000

    added a utility folder for commonly used functions, such as converting between (row, col) to grid index

--- include/utils/conversion.h
+++ include/utils/conversion.h
@@ -2,6 +2,6 @@
 #define CONVERSION_H_
 
 short toIndex(char row, char col, char gridSize);
-void toRowCol(char index, char gridSize, char& row, char& col);
+void toRowCol(short index, char gridSize, char& row, char& col);
 
 #endif
--- src/utils/conversion.cpp
+++ src/utils/conversion.cpp
@@ -4,7 +4,7 @@ short toIndex(char row, char col, char gridSize) {
     return ((short)row * gridSize) + col;
 }
 
-void toRowCol(char index, char gridSize, char& row, char& col) {
+void toRowCol(short index, char gridSize, char& row, char& col) {
     row = index / gridSize;
     col = index % gridSize;
 }
