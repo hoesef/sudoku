627c907211979ba865792022ab2f6cbbb53b969a
Joe <joe.luke.manning@gmail.com>
2025-03-20 17:51:53 +0000

    added a copy constructor and assignment operator to ruleset

--- include/ruleSet.h
+++ include/ruleSet.h
@@ -8,11 +8,14 @@ class RuleSet {
 
     private:
         SudokuRules** mRuleSet{nullptr};
-        int mNumRules{0};
+        int mNumRules = 0;
     
     public:
-        RuleSet() {}
+        RuleSet() {std::cout << "Created ruleset object\n\n";}
+        RuleSet(const RuleSet& other);
         RuleSet(SudokuRules** rules, int numRules);
+        RuleSet& operator=(const RuleSet& other);
+        int getNum() {return mNumRules;}
         bool applyRules(const char value, const short index, char*& board, char gridSize);
         ~RuleSet();
 
--- src/ruleSet.cpp
+++ src/ruleSet.cpp
@@ -1,22 +1,53 @@
 #include "..\include\ruleSet.h"
 
+RuleSet::RuleSet(const RuleSet& other) {
+    mNumRules = other.mNumRules;
+    mRuleSet = new SudokuRules*[mNumRules];
+    for (int i = 0; i < mNumRules; i++) {
+        std::cout << "Address of rule being coppied: " << other.mRuleSet[i] << "\n";
+        mRuleSet[i] = (other.mRuleSet[i])->clone();   
+        std::cout << "Address of new rule: " << mRuleSet[i] << "\n";
+    }
+}
 RuleSet::RuleSet(SudokuRules** rules, int numRules) {
     mNumRules = numRules;
     mRuleSet = new SudokuRules*[mNumRules];
-
-    std::cout << "RULE SET CODE SHOULD BE UPDATED:\nPotential Memory Leak" << std::endl;
-
-    // DOES NOT TAKE OWNERSHIP OF THE OBJECT
-    // CAN CAUSE MEMORY LEAKS
-    // SHOULD ADD A COPY CONSTRUCTOR IN SUDOKURULES
     for (int i = 0; i < mNumRules; i++) {
-        mRuleSet[i] = rules[i];
+        mRuleSet[i] = rules[i]->clone();
     }
+}
+RuleSet& RuleSet::operator=(const RuleSet& other) {
+    // Delete any perviously alocated rules
+    std::cout << "NumRules: " << mNumRules << "\n";
+    for (int i = 0; i < mNumRules; i++) {
+        delete mRuleSet[i];
+        mRuleSet[i] = nullptr;
+    }
+    delete [] mRuleSet;
+    mRuleSet = nullptr;
+
+    std::cout << "Deleted old pointers\n";
+    // Set new rules
+    mNumRules = other.mNumRules;
+    std::cout << "num rules coppied\n";
+    mRuleSet = new SudokuRules*[mNumRules];
+    std::cout << "Memory for mRuleSet allocated\n";
+
+    for (int i = 0; i < mNumRules; i++) {
+        *(mRuleSet[i]) = *(other.mRuleSet[i]);
+        std::cout << "Address of copied rule: " << other.mRuleSet[i] << "\nAddress of new rule: " << mRuleSet[i] << "\n\n";
+    }
+    std::cout << "Rules coppied\n\n";
+    return *this;
 }
 bool RuleSet::applyRules(const char value, const short index, char*& board, char gridSize) {
     bool valid = false;
+    std::cout << "In apply rules\n";
     for (int i = 0; i < mNumRules; i++) {
+        std::cout << "Checking rule: " << i << "\n";
+        std::cout << mRuleSet[i]->getName();
         valid = (mRuleSet[i])->isValid(value, index, board, gridSize);
+        std::cout << (valid?"True\n":"False\n");
         if (!valid) { return false; }
     }
     return true;
